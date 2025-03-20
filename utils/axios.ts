6bd42ec1f644be82ea56b362d01195a9ad0190f7
vignesh17-k <vignesh@sourcewiz.co>
2025-03-20 22:11:55 +0530

    fix: send token to req

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import { store } from '../store/store';
+import { get } from 'lodash';
 
 
 const axiosInstance = axios.create({
   baseURL: 'https://food-app-backend-flame-omega.vercel.app',
-	timeout: 3600 * 1000,
+  timeout: 3800 * 1000,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -11,11 +13,15 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    // Add auth token dynamically if needed
-    // const token = localStorage.getItem('authToken');
-    // if (token) {
-    //   config.headers['Authorization'] = `Bearer ${token}`;
-    // }
+    const new_state = store.getState();
+    const token = get(new_state, 'user.session.session.access_token');
+    console.log(token, "token from store exist");
+    if (token) {
+      config.headers.authorization = `Bearer ${token}`;
+      config.headers['x-client-id'] = 'WEB';
+      config.headers['x-client-version'] = '0.0.1';
+      config.headers['x-client-env'] = 'DEV';
+    }
     return config;
   },
   (error) => {
